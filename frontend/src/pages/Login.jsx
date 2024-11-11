241bd0d3197b46b51dfafd51ce01762de7b0fd42
Nithesh G <158061090+Nithesh04@users.noreply.github.com>
2024-11-11 21:51:21 +0530

    first commit

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -55,7 +55,7 @@ function Login() {
         <div className="w-full max-w-md bg-white shadow-md rounded-lg p-8">
           <form onSubmit={handleLogin}>
             <div className="font-semibold text-xl items-center text-center">
-              <span className="text-red-500">Login</span>
+              <span className="text-red-500">Login </span>
             </div>
           
             <select
